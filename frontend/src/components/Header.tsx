c30a43ac9d777934671f34b9c856ec234b8f5e96
VRJayasri <159994876+VRJayasri@users.noreply.github.com>
2026-01-26 15:49:45 +0530

    dashboard ui

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="flex items-center gap-2 bg-yellow-200 px-4 py-3 shadow-md">
       <img src="src/assets/logo.svg" alt="logo" className="w-13 h-14" />
 
-      <div className="font-semibold text-[25px] text-black">Money tracker</div>
+      <div className="font-extrabold text-[25px] text-black">Money tracker</div>
     </header>
   );
 };
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,19 @@
+import Dashboard from "../components/Dashboard";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="p-6 bg-yellow-200 space-y-6">
+      <Dashboard />
+      <div className="p-6 bg-yellow-200 space-y-6">
+        {/* Header */}
+        <div className="w-full bg-yellow-100/70 rounded-none shadow p-4">
+          <h4 className="text-xl font-semibold text-left text-gray-900">
+            Plans
+          </h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
